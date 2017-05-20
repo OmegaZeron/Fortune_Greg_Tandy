54dfe0289e2788f9b050af24ab68642f8e2b32f9
Unknown <omegazeron@yahoo.com>
2017-05-19 21:09:33 -0700

    Set up enabled buttons

--- fortuneclient/addfortunebox.cpp
+++ fortuneclient/addfortunebox.cpp
@@ -6,6 +6,7 @@ AddFortuneBox::AddFortuneBox(QWidget *parent) :
     ui(new Ui::AddFortuneBox)
 {
     ui->setupUi(this);
+    ui->sendButton->setEnabled(false);
 }
 
 AddFortuneBox::~AddFortuneBox()
@@ -23,6 +24,7 @@ void AddFortuneBox::on_sendButton_clicked()
     //server QDataStream fortunes << addFortune stuff
     ui->lineEdit->setText("");
     ui->confirmLabel->setText("Fortune Added!"); // need to make this wait until server accepts in case of connection error
+    ui->sendButton->setEnabled(false);
 }
 
 void AddFortuneBox::on_lineEdit_editingFinished()
@@ -35,9 +37,11 @@ void AddFortuneBox::on_lineEdit_returnPressed()
     //same as sendButton_clicked()
     ui->lineEdit->setText("");
     ui->confirmLabel->setText("Fortune Added!"); // need to make this wait until server accepts in case of connection error
+    ui->sendButton->setEnabled(false);
 }
 
 void AddFortuneBox::on_lineEdit_textChanged(const QString &arg1)
 {
     ui->confirmLabel->setText("");
+    ui->sendButton->setEnabled(true);
 }
