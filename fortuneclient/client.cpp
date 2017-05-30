fcdc39ff781c955f488c0219451edf0c46b0d2f2
tdang93 <aznnoodle101@aol.com>
2017-05-29 20:23:06 -0700

    Almost+
    got two lines of that program-crashing code working. now just 2 more...
    ><

--- fortuneclient/client.cpp
+++ fortuneclient/client.cpp
@@ -132,7 +132,35 @@ Client::Client(QWidget *parent)
         getFortuneButton->setEnabled(false);
         statusLabel->setText(tr("Opening network session."));
         networkSession->open();
+    } else {
+        sessionOpened();
     }
+
+    //===================================================================
+    /*
+    if (manager.capabilities() & QNetworkConfigurationManager::NetworkSessionRequired) {
+        // Get saved network configuration
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+        settings.endGroup();
+
+        // If the saved network configuration is not currently discovered use the system default
+        QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+        if ((config.state() & QNetworkConfiguration::Discovered) !=
+                QNetworkConfiguration::Discovered) {
+            config = manager.defaultConfiguration();
+        }
+
+        networkSession = new QNetworkSession(config, this);
+        connect(networkSession, &QNetworkSession::opened, this, &Server::sessionOpened);
+
+        statusLabel->setText(tr("Opening network session."));
+        networkSession->open();
+    } else {
+        sessionOpened();
+    }
+    */
 }
 
 void Client::requestNewFortune()
@@ -209,6 +237,7 @@ void Client::enableAddButton()
 
 void Client::sessionOpened()
 {
+    /*
     // Save the used configuration
     QNetworkConfiguration config = networkSession->configuration();
     QString id;
@@ -226,6 +255,49 @@ void Client::sessionOpened()
                             "Fortune Server example as well."));
 
     enableGetFortuneButton();
+    */
+
+    //=============================================================
+
+    // Save the used configuration
+    if (networkSession) {
+        QNetworkConfiguration config = networkSession->configuration();
+        QString id;
+        if (config.type() == QNetworkConfiguration::UserChoice)
+            id = networkSession->sessionProperty(QLatin1String("UserChoiceConfiguration")).toString();
+        else
+            id = config.identifier();
+
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+        settings.endGroup();
+    }
+
+    tcpServer = new QTcpServer(this);
+    if (!tcpServer->listen()) {
+        QMessageBox::critical(this, tr("Fortune Server"),
+                              tr("Unable to start the server: %1.")
+                              .arg(tcpServer->errorString()));
+        close();
+        return;
+    }
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+    // use the first non-localhost IPv4 address
+    for (int i = 0; i < ipAddressesList.size(); ++i) {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
+                ipAddressesList.at(i).toIPv4Address()) {
+            ipAddress = ipAddressesList.at(i).toString();
+            break;
+        }
+    }
+    // if we did not find one, use IPv4 localhost
+    if (ipAddress.isEmpty())
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+    statusLabel->setText(tr("The server is running on\n\nIP: %1\nport: %2\n\n"
+                            "Run the Fortune Client example now.")
+                         .arg(ipAddress).arg(tcpServer->serverPort()));
 }
 void Client::setNewFortune()
 {
@@ -243,11 +315,10 @@ void Client::addNewFortune()
 
     out << newFortune;
 
-// this crashes the program
-//    QTcpSocket *hostConnection = tcpServer->nextPendingConnection();
-//    connect(hostConnection, &QAbstractSocket::disconnected,
-//            hostConnection, &QObject::deleteLater);
-
+// this crashes the program // added stuff in client.cpp constructor from server.cpp constructor to try to make it work
+    QTcpSocket *hostConnection = tcpServer->nextPendingConnection(); // this line works now at least...
+    connect(hostConnection, &QAbstractSocket::disconnected,
+            hostConnection, &QObject::deleteLater);
 //    hostConnection->write(block);
 //    hostConnection->disconnectFromHost();
 
